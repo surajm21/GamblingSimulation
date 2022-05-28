789512def5250c08a8c4375b07da18d8494a2ade
suraj <surajmore2143@gmail.com>
2022-05-28 11:08:26 +0530

    Added to gambler make either win or loss amount feature

--- Gambler.java
+++ Gambler.java
@@ -2,8 +2,19 @@ package com.bridglabz;
 
 public class Gambler {
 
-    public static void main(String[] args) {
+    public static void startGambling(){
         int StartingAmount = 100;
         int BetPerAmount = 1;
+        int BetResult = (int) Math.floor(Math.random() * 10) % 2 ;
+            if ( BetResult == BetPerAmount){
+
+                System.out.println("win");
+            }
+            else
+                System.out.println("loos");
+    }
+
+    public static void main(String[] args) {
+        startGambling();
     }
 }
